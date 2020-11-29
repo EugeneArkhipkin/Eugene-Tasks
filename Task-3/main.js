8c96528496969f9973d60d72da1c868638188a06
Eugene <khrist1989@gmail.com>
2020-11-29 17:28:05 +0200

    Dev Task-3,4

---
+++ Task-3/main.js
@@ -0,0 +1,6 @@
+   var num1 = +prompt ("Введите число");
+   var num2 = +prompt ("Еще раз введите число");
+   var num3 = +prompt ("И последний разочек");
+   var operacia = prompt ('Выберите арифметическую операцию +-/*');
+   var res=num1+num2+num3;
+   alert(res);
---
+++ Task-4/main.js
@@ -0,0 +1,3 @@
+   var names = ["Alex","Sergei","Olga","Denis"];
+   names.splice (0,1, "Roman");
+   console.log(names);
