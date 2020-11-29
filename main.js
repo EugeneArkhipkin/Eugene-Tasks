d9d4b2135640c47a2675caa4719a4ab504571b91
Eugene <khrist1989@gmail.com>
2020-11-29 17:07:28 +0200

    Dev Task-1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var answer = prompt ("Введите имя");
+  var answer = prompt ("Введите имя");
   var answer2 = prompt ("Введите дату рождения");
   alert ("Имя " + answer);
   alert ("Возвраст " + answer2);
